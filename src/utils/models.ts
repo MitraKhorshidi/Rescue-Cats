b0494add43ae884012f0354571cc442120c827db
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-09-07 17:04:10 +0200

    define Breed interface

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -6,3 +6,18 @@ export interface Cat{
     gender ?: string,
     img ?:string,
 }
+
+export interface Breed {
+    
+    id ?: string
+    name ?: string
+    origin ?: string
+    description ?: string
+    life_span ?: string
+    child_friendly ?: number,
+    dog_friendly ?: number,
+    adaptability ?: number,
+    energy_level ?: number,
+    health_issues ?: number,
+    intelligence ?:number,
+}
