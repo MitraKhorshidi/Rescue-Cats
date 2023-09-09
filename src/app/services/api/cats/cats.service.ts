6cf304fd8fb9f553590d038608ac1f9b45f309bb
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-09-09 21:06:23 +0200

    use fake db for cats

--- src/app/services/api/cats/cats.service.ts
+++ src/app/services/api/cats/cats.service.ts
@@ -3,21 +3,25 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cat } from 'src/utils/models';
 
+import cats from 'src/assets/data';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CatsService {
 
-  url:string='/assets/data.json';
+  catsList: Cat[] = [];
 
-  constructor(private httpClient:HttpClient) { }
+  constructor() { }
 
-  getAllCats():Observable<Cat[]>{
-    return this.httpClient.get<Cat[]>(this.url);
+  getAllCats(): Observable<Cat[]> {
+    return new Observable(observer => observer.next(cats));
   }
-  // getCat(id:number):Observable<Cat>{
-  //   this.httpClient.get<Cat[]>(this.url);
-  // }
+
+  getCat(id: number): Observable<Cat> {
+    const cat = this.catsList.find((cat) => cat.id == id);
+    return new Observable(observer => observer.next(cat));
+  }
 
 
 }
