3b7339562e788ee838aa4fe3be021de3dbb62aee
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-09-10 16:21:05 +0200

    design cats detail page

--- src/app/cat-detail/cat-detail.component.ts
+++ src/app/cat-detail/cat-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CatsService } from '../services/api/cats/cats.service';
-import { Cat } from 'src/utils/models';
+import { Breed, Cat } from 'src/utils/models';
 import { BreedsService } from '../services/api/breeds/breeds.service';
 
 @Component({
@@ -9,33 +9,41 @@ import { BreedsService } from '../services/api/breeds/breeds.service';
   templateUrl: './cat-detail.component.html',
   styleUrls: ['./cat-detail.component.css']
 })
-export class CatDetailComponent implements OnInit{
+export class CatDetailComponent implements OnInit {
 
-  cat :Cat ={} as Cat;
+  cat: Cat = {} as Cat;
+  breed: Breed = {} as Breed;
 
   constructor(
-    private router : Router,
-    private activatedRout:ActivatedRoute,
-    private catsService :CatsService,
-    private breedsService : BreedsService,
-    ){}
+    private router: Router,
+    private activatedRout: ActivatedRoute,
+    private catsService: CatsService,
+    private breedsService: BreedsService,
+  ) { }
 
   ngOnInit(): void {
     const catId: string = this.activatedRout.snapshot.params['catId'];
     this.catsService.getCat(catId).subscribe({
-      next:(result:Cat)=>{
+      next: (result: Cat) => {
         console.log(result);
-        this.cat=result;
+        this.cat = result;
       }
     });
-    
+
+    const breedId: string = this.cat.breedId;
+    this.breedsService.getBreed(breedId).subscribe({
+      next: (result: Breed) => {
+        this.breed = result;
+      }
+    });
+
 
   }
 
-  onClick(){
-    this.router.navigate(["adopt",this.cat.id]);
+  onClick() {
+    this.router.navigate(["adopt", this.cat.id]);
   }
-  catSelected(cat:Cat){}
+  catSelected(cat: Cat) { }
 
 
 
