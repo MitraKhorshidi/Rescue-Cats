7d0c45e3e91fc08e740ea6fd4df882d1b356f279
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-09-11 15:38:04 +0200

    fix links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CatsComponent } from '../cats/cats.component';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +9,6 @@ import { CatsComponent } from '../cats/cats.component';
 export class HomeComponent {
   heroImg: string = '/assets/images/hero.jpg';
 
-  constructor(private router: Router) { }
 
-  goCats() {
-    this.router.navigate(['cats']);
-  }
-  goDonates() {
-    this.router.navigate(['donate']);
-  }
+
 }
