8e93712d64fafe23f0d8afb84219fb6102bb682d
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-09-11 14:19:58 +0200

    designing home

--- src/app/breeds/breeds.component.ts
+++ src/app/breeds/breeds.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Breed } from 'src/utils/models';
 import { BreedsService } from '../services/api/breeds/breeds.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-breeds',
@@ -9,18 +10,27 @@ import { BreedsService } from '../services/api/breeds/breeds.service';
 })
 export class BreedsComponent implements OnInit {
 
+  // pageNum :any ;
+
   breedsList: Breed[] = [];
 
   constructor(
     private breedsService: BreedsService,
+    // private activatedRoute :ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
     this.breedsService.getAllBreeds().subscribe({
       next: (result: Breed[]) => {
-        console.log(result);
+        // console.log(result);
         this.breedsList = result;
       }
     });
+
+    // console.log('queryparam',this.activatedRout.snapshot.queryParamMap.get('pageNum') );
+    // this.activatedRoute.queryParamMap.subscribe((params) => {
+    //   this.pageNum = params.get('pageNum') ;
+    //   console.log('Query params ', this.pageNum);
+    // });
   }
 }
